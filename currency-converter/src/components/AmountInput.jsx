2e5587384481a66f16cc9de829bc1ad03ce52463
Sami <samiberakih3@gmail.com>
2024-10-05 14:34:06 +0300

    add dark mode

--- currency-converter/src/components/AmountInput.jsx
+++ currency-converter/src/components/AmountInput.jsx
@@ -7,46 +7,53 @@ const Currencydropdown = ({
   favorites,
   handleFavorite,
   title = "",
+  isDarkMode, // Receive the isDarkMode prop
 }) => {
   const isFavorite = (curr) => favorites.includes(curr);
 
   return (
     <div>
-      <label htmlFor={title} className="block text-sm font-medium text-gray-700">
+      <label htmlFor={title} className={`block text-sm font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
         {title}
       </label>
 
       <div className="mt-1 relative">
-        {/* Container wrapping the select and star icon */}
         <div className="relative">
           <select
             value={currency}
             onChange={(e) => setCurrency(e.target.value)}
-            className="w-full p-2 pr-10 border border-cyan-500 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-cyan-600"
+            className={`w-full p-2 pr-10 border rounded-lg shadow-lg focus:outline-none focus:ring-2 ${
+              isDarkMode ? "bg-gray-700 border-gray-600 text-white" : "border-cyan-500"
+            }`}
           >
             {/* Favorites */}
-            {favorites.map((currency) => {
-              return (
-                <option className="bg-gray-200" value={currency} key={currency}>
-                  {currency}
-                </option>
-              );
-            })}
+            {favorites.map((curr) => (
+              <option className="bg-gray-200" value={curr} key={curr}>
+                <img
+                  src={`https://flagcdn.com/${curr.toLowerCase()}.svg`}
+                  alt={`${curr} flag`}
+                  style={{ width: "20px", marginRight: "8px" }}
+                />
+                {curr}
+              </option>
+            ))}
             <hr />
 
             {/* Remaining currencies */}
             {currencies
               .filter((c) => !favorites.includes(c))
-              .map((currency) => {
-                return (
-                  <option value={currency} key={currency}>
-                    {currency}
-                  </option>
-                );
-              })}
+              .map((curr) => (
+                <option value={curr} key={curr}>
+                  <img
+                    src={`https://flagcdn.com/${curr.toLowerCase()}.svg`}
+                    alt={`${curr} flag`}
+                    style={{ width: "20px", marginRight: "8px" }}
+                  />
+                  {curr}
+                </option>
+              ))}
           </select>
 
-          {/* Star icon positioned inside the select */}
           <button
             onClick={() => handleFavorite(currency)}
             className="absolute inset-y-0 right-6 flex items-center text-sm leading-5"
